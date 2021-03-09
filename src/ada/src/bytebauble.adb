e4f48e3068ce12d401260fd4e2fd5f0fd290517e
Maya Posch <maya@nyanko.ws>
2021-03-09 17:28:03 +0100

    Add initial Ada port with byte swaps.

--- src/ada/src/bytebauble.adb
+++ src/ada/src/bytebauble.adb
@@ -1,19 +1,107 @@
--- bytebauble.adb - Body for the ByteBauble package.
+--
+-- bytebauble.adb - Body of the ByteBauble package.
+--
+--
+-- 2021/03/05, Maya Posch
 
--- Revision 0
 
--- Features:
---		- 
-
--- Notes:
---		- 
-
--- 2019/06/10, Maya Posch
+with Ada.Text_IO; use Ada.Text_IO;
+with System; use System;
 
 
 package body ByteBauble is
---
-
-begin
+	-- DETECT HOST ENDIAN --
+	procedure detectHostEndian is
+	-- type uint16 is mod 2 ** 16;
+	-- type uint8 is mod 2 ** 8;
+	-- bytes	: uint16	:= 1;
+	-- check	: uint8;
+	begin
+		--check := bytes(0 .. 7);
+		Put_Line ("Endianness " & Bit_Order'Image(Default_Bit_Order));
+		--if check > 0 then
+		if Default_Bit_Order = LOW_ORDER_FIRST then
+			hostEndian := BB_LE;
+			Put_Line(Standard_Output, "Detected Host Little Endian.");
+		else
+			hostEndian := BB_BE;
+			Put_Line(Standard_Output, "Detected Host Big Endian.");
+		end if;
+	end detectHostEndian;
 	
+	
+	-- GET HOST ENDIAN --
+	function getHostEndian return BBEndianness is 
+	begin
+		return hostEndian;
+	end getHostEndian;
+	
+	
+	-- SET GLOBAL ENDIANNESS --
+	procedure setGlobalEndianness(endian : in BBEndianness) is
+	begin
+		globalEndian := endian;
+	end setGlobalEndianness;
+	
+	
+	function toGlobal(value : in uint16; endian : in BBEndianness) return uint16 is
+	begin
+		if globalEndian = endian then
+			-- -- Same endianness, just return the value.
+			return value;
+		end if;
+		
+		return Bswap_16(value);
+	end toGlobal;
+	
+	function toGlobal(value : in uint32; endian : in BBEndianness) return uint32 is
+	begin
+		if globalEndian = endian then
+			-- -- Same endianness, just return the value.
+			return value;
+		end if;
+		
+		return Bswap_32(value);
+	end toGlobal;
+	
+	function toGlobal(value : in uint64; endian : in BBEndianness) return uint64 is
+	begin
+		if globalEndian = endian then
+			-- -- Same endianness, just return the value.
+			return value;
+		end if;
+		
+		return Bswap_64(value);
+	end toGlobal;
+	
+	
+	function toHost(value : in uint16; endian : in BBEndianness) return uint16 is
+	begin
+		if hostEndian = endian then
+			-- Same endianness, just return the value.
+			return value;
+		end if;
+		
+		return Bswap_16(value);
+	end toHost;
+	
+	function toHost(value : in uint32; endian : in BBEndianness) return uint32 is
+	begin
+		if hostEndian = endian then
+			-- Same endianness, just return the value.
+			return value;
+		end if;
+		
+		return Bswap_32(value);
+	end toHost;
+	
+	function toHost(value : in uint64; endian : in BBEndianness) return uint64 is
+	begin
+		if hostEndian = endian then
+			-- Same endianness, just return the value.
+			return value;
+		end if;
+		
+		return Bswap_64(value);
+	end toHost;
 end ByteBauble;
--- src/ada/src/bytebauble.ads
+++ src/ada/src/bytebauble.ads
@@ -1,30 +1,44 @@
+--
 -- bytebauble.ads - Specification for the ByteBauble package.
+--
+-- 2021/03/05, Maya Posch
+--
 
--- Revision 0
-
--- Features:
---		- 
-
--- Notes:
---		- 
-
--- 2019/06/10, Maya Posch
-
-
+with Interfaces;
 
 package ByteBauble is
-	type BBEndianness is (BB_LE, BB_BE);
+	type BBEndianness is (BB_BE, BB_LE);
 	
-	type ByteBauble is tagged record;
-		--
-		globalEndian: BBEndianness := BB_LE;
-		hostEndian: BBEndianness := detectHostEndian;
-	end record;
+	subtype uint16  is Interfaces.Unsigned_16;
+	subtype uint32  is Interfaces.Unsigned_32;
+	subtype uint64  is Interfaces.Unsigned_64;
+	--subtype uint128  is Interfaces.Unsigned_128;
 	
+	function Bswap_16 (X : uint16) return uint16;
+	pragma Import (Intrinsic, Bswap_16, "__builtin_bswap16");
 
-	function detectHostEndian return BBEndianness;
-	procedure detectHostEndian(Self: in out ByteBauble);
-	procedure setGlobalEndianness(Self: in out ByteBauble, endian: in BBEndianness);
-	
+	function Bswap_32 (X : uint32) return uint32;
+	pragma Import (Intrinsic, Bswap_32, "__builtin_bswap32");
 
+	function Bswap_64 (X : uint64) return uint64;
+	pragma Import (Intrinsic, Bswap_64, "__builtin_bswap64");
+
+	-- function Bswap_128 (X : uint128) return uint128;
+	-- pragma Import (Intrinsic, Bswap_128, "__builtin_bswap128");
+	
+	procedure detectHostEndian;
+	function getHostEndian return BBEndianness;
+	procedure setGlobalEndianness(endian : in BBEndianness);
+	
+	function toGlobal(value : in uint16; endian : in BBEndianness) return uint16;
+	function toGlobal(value : in uint32; endian : in BBEndianness) return uint32;
+	function toGlobal(value : in uint64; endian : in BBEndianness) return uint64;
+	
+	function toHost(value : in uint16; endian : in BBEndianness) return uint16;
+	function toHost(value : in uint32; endian : in BBEndianness) return uint32;
+	function toHost(value : in uint64; endian : in BBEndianness) return uint64;
+	
+private
+	globalEndian	: BBEndianness	:= BB_LE;
+	hostEndian		: BBEndianness	:= BB_LE;
 end ByteBauble;
